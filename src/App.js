7fac9ae188de5b511a8d944a1d6c36fb48d23eda
mangesh <106296301+mangesh16cloud@users.noreply.github.com>
2022-08-23 10:16:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code> Bhumi Itech Pvt Ltd</code> sample App.
+           Bhumi Itech <code> React </code> sample App.
         </p>
         <a
           className="App-link"
