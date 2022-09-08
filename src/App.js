2758574745872efa3d84eb5529f7b780c926baf2
mangesh <106296301+mangesh16cloud@users.noreply.github.com>
2022-09-08 14:05:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           React js app <code> React </code> .
+           BHUMI js app <code> React </code> .
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          To know more about click here
+          To know more about BHUMI click here
         </a>
       </header>
     </div>
